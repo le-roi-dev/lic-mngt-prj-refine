8a8bda5d1a6f55a2ad82dde869de0272e2021d0e
le-roi-dev <chry.dev1220@outlook.com>
2024-07-31 18:06:06 +0900

    rechange the api

--- src/providers/data-provider/index.ts
+++ src/providers/data-provider/index.ts
@@ -5,6 +5,6 @@ import dataProviderSimpleRest from "@refinedev/simple-rest";
 const API_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000/api"
-    : "https://lic-refine.verce.app/";
+    : "https://lic-refine.verce.app/api";
 
 export const dataProvider = dataProviderSimpleRest(API_URL);
