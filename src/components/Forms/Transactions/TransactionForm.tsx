cc3843290eb17bd43653d7ac6a659bcfc0765820
le-roi-dev <chry.dev1220@outlook.com>
2024-07-31 23:48:28 +0900

    some improvement of transaction Form

--- src/components/Forms/Transactions/TransactionForm.tsx
+++ src/components/Forms/Transactions/TransactionForm.tsx
@@ -6,18 +6,32 @@ import { Transaction } from "@/types/types";
 import TransactionFormFields from "./TransactionFormFields";
 import PartnerFormFields from "../Partners/PartnerFormFields";
 import GenericForm from "../GenericForm";
+import { Divider } from "@mui/material";
 
 export type TransactionFormProps = GenericFormProps & {
   transaction?: Transaction;
+};
+const dividerStyle = {
+  fontSize: '1.5rem',
+  fontWeight: 'medium'
 };
 
 const TransactionForm = (props: TransactionFormProps) => {
 
   return (
     <div className="flex flex-col gap-6">
-      <GenericForm {...{ ...props, fields: TransactionFormFields }} />
-      <GenericForm {...{ ...props, fields: PartnerFormFields.BillingPartnerInformationFormFields }} />
-      <GenericForm {...{ ...props, fields: PartnerFormFields.ShippingPartnerInformationFormFields }} />
+      <Divider sx={dividerStyle}>Transaction</Divider>
+      <div className="px-8 py-8">
+        <GenericForm {...{ ...props, fields: TransactionFormFields }} />
+      </div>
+      <Divider sx={dividerStyle}>Billing Partner Information</Divider>
+      <div className="px-8 py-8">
+        <GenericForm {...{ ...props, fields: PartnerFormFields.BillingPartnerInformationFormFields }} />
+      </div>
+      <Divider sx={dividerStyle}>Shipping Partner Information</Divider>
+      <div className="px-8 py-8">
+        <GenericForm {...{ ...props, fields: PartnerFormFields.ShippingPartnerInformationFormFields }} />
+      </div>
     </div>
   );
 };
